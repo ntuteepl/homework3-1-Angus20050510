af53581662b1b56228fcf88863d184306877ba6e
Angus20050510 <145432971+Angus20050510@users.noreply.github.com>
2023-10-22 23:31:08 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+main()
+{
+int s1,d1,s2,d2,s3,d3,s;
+scanf("%d%d&d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
+if(s2<d1){
+    s++;
+}
+if(s3<d2){
+    s++;
+}
+if(s3<d1){
+    s++;
+}
+printf("%d\n",s);
+return 0;
+}
