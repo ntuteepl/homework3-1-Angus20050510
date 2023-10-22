92bf90402e36c890971d71a57b55f1ef54afe63d
Angus20050510 <145432971+Angus20050510@users.noreply.github.com>
2023-10-22 22:41:08 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+main()
+{
+    int i,k;
+    scanf("%d",&i);
+    if(i>1){
+        for(int j=2;j<i;j++){
+            if(i%j==0){
+                k=0;
+                }
+            else{
+                k=1;
+                }
+            }
+
+    }
+    else
+        printf("NO");
+
+    //printf("%d",k);
+    if(k==0){
+        printf("NO");
+    }
+    else if(k==1){
+        printf("YES");
+    }
+}
