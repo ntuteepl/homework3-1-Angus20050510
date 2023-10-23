506867637013dd948f0ac6e28baa9c134c122d52
Angus20050510 <145432971+Angus20050510@users.noreply.github.com>
2023-10-23 13:28:40 +0800

    Update hw3-4.c

--- hw3-4.c
+++ hw3-4.c
@@ -1,1 +1,28 @@
 
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char answer[] = "1234";
+    char guess[5]; 
+    int A = 0, B = 0; 
+    
+    scanf("%s", guess);
+
+    for(int i = 0; i < 4; i++) {
+        if(guess[i] == answer[i]) {
+            A++;
+        } else {
+            for(int j = 0; j < 4; j++) {
+                if(guess[i] == answer[j]) {
+                    B++;
+                }
+            }
+        }
+    }
+
+    printf("%dA%dB\n", A, B);
+
+
+    return 0;
+}
